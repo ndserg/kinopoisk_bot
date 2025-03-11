7ed446010c26bc26e01b57584f003ea0ea83f504
Denis <ndserg@gmail.com>
2025-03-11 17:09:08 +0300

    Добавит исключение при не удачной попытке удалить старые сообщения

--- handlers/utils/delete_cancel_msgs.py
+++ handlers/utils/delete_cancel_msgs.py
@@ -1,7 +1,11 @@
+from aiogram.exceptions import TelegramAPIError
 from aiogram.types import CallbackQuery
 
 
 async def delete_cancel_msgs(
     caller: CallbackQuery, msg_ids: list[int], chat_id: int | str
 ) -> None:
-    await caller.bot.delete_messages(message_ids=msg_ids, chat_id=chat_id)
+    try:
+        await caller.bot.delete_messages(message_ids=msg_ids, chat_id=chat_id)
+    except TelegramAPIError as e:
+        print(f"Не удалось удалить сообщение: {e}")
